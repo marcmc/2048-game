48c1c1520325b99d2529cab5b823d4904a443341
marcmc <marcmcsarri@gmail.com>
2014-05-28 21:41:25 +0200

    Edited the main code
    Edited the main code gotten from internet to adapt to my program and
    added the background images

--- 2048.py
+++ 2048.py
@@ -1,20 +1,15 @@
 import sys, pygame
-init()
+pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+size = width, height = 480, 476
 screen = pygame.display.set_mode(size)
-ball = pygame.image.load("ball.bmp")
-ballrect = ball.get_rect()
+background = pygame.image.load('taulell.bmp').convert()
+screen.blit(background, (0, 0))
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-        screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+	
+    pygame.display.update()
+    pygame.time.delay(100)
+
---
+++ geany_run_script.bat
@@ -0,0 +1,6 @@
+python "2048.py"
+
+pause
+del "%0"
+
+pause
